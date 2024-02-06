115c6e3f6d0273124faa3700be21985a6949784d
Borb <66694597+MFBorb@users.noreply.github.com>
2024-02-06 13:57:38 -0500

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool dogSpawnable = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && dogSpawnable)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogSpawnable = false;
+            StartCoroutine(ResetDogBooleanAfterTime());
+        }
+    }
+
+    IEnumerator ResetDogBooleanAfterTime() {
+        yield return new WaitForSeconds(1.75f);
+
+        dogSpawnable = true;
+    }
+}
